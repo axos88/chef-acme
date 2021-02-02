95423974ee0f519f49fc1d9d984b5109c921a62d
Akos Vandra <axos88@gmail.com>
2021-02-02 08:17:37 +0100

    delete any old acme challenge records during setup

--- libraries/ssl_certificate/digital_ocean_dns.rb
+++ libraries/ssl_certificate/digital_ocean_dns.rb
@@ -23,6 +23,14 @@ class Chef
           domain = fqdn.split('.')[-2..-1].join('.')
           record_name = fqdn.split('.')[0...-2].join('.')
 
+          all_records = client.domain_records.all(for_domain: domain)
+          matching_records = all_records.select { |r| r['name'] == record_name && r['type'] == challenge.record_type }
+
+          matching_records.each do |r|
+            Chef::Log.info("Deleting challenge DNS record for domain #{domain} #{r.to_h}")
+            client.domain_records.delete(for_domain: domain, id: r.id)
+          end
+
           record = DropletKit::DomainRecord.new(
             type: challenge.record_type,
             name: record_name,
